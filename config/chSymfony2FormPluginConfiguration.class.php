92736d154a92c6f1d4d89b6b05cf73c796c7d060
Fabien Pomerol <fabien.pomerol@gmail.com>
2012-02-27 18:39:19 +0100

    Added plugin configuration file

---
+++ config/chSymfony2FormPluginConfiguration.class.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * chSymfony2FormPluginConfig configuration
+ *
+ * @package    chSymfony2FormPlugin
+ * @subpackage config
+ * @author     Fabien Pomerol <fabien.pomerol@gmail.com>
+ * @copyright (c) Carpe Hora SARL 2012
+ * @since 2012-02-24
+ */
+
+//namespace chSymfony2FormPlugin\config;
+
+class chSymfony2FormPluginConfiguration extends sfPluginConfiguration
+{
+  const VERSION = '1.0.0-DEV';
+  /**
+  * @see sfPluginConfiguration
+  */
+  public function initialize()
+  {
+    spl_autoload_register(array($this, 'autoloadNamespace'));
+  }
+
+  public function autoloadNamespace($className)
+  {
+    $className = ltrim($className, '\\');
+    $fileName = '';
+    $namespace = '';
+    if ($lastNsPos = strripos($className, '\\'))
+    {
+      $namespace = substr($className, 0, $lastNsPos);
+      $className = substr($className, $lastNsPos + 1);
+      $fileName = str_replace('\\', DIRECTORY_SEPARATOR, $namespace) . DIRECTORY_SEPARATOR;
+    }
+
+    foreach(array(
+      sfConfig::get('sf_plugins_dir'). '/' . DIRECTORY_SEPARATOR . $fileName . $className . '.php',
+      sfConfig::get('sf_lib_dir').'/'. DIRECTORY_SEPARATOR . $fileName . $className . '.php',
+      sfConfig::get('sf_plugins_dir').'/chSymfony2FormPlugin/vendor'. DIRECTORY_SEPARATOR . $fileName . $className . '.php',
+      ) as $fileName)
+    {
+      if (file_exists($fileName))
+      {
+        require $fileName;
+        return true;
+      }
+    }
+    return false;
+  }
+
+
+
+}
