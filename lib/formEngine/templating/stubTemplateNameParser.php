fd2e0a95c7cf5edee97ef9ca0eb2927c9d26c3d1
Fabien Pomerol <fabien.pomerol@gmail.com>
2012-02-28 10:04:26 +0100

    Added formEngine templating

---
+++ lib/formEngine/templating/stubTemplateNameParser.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * This file declare the StubTemplateNameParser class
+ *
+ * @package chSymfony2FormPlugin
+ * @subpackage templating
+ * @author Fabien Pomerol <fabien_pomerol@gmail.com>
+ * @copyright (c) Carpe Hora SARL 2012
+ * @since 2012-02-23
+ */
+
+namespace chSymfony2FormPlugin\lib\formEngine\templating;
+
+use Symfony\Component\Templating\TemplateReference;
+use Symfony\Component\Templating\TemplateNameParserInterface;
+
+/**
+ * Template name parser
+ * @see Symfony\Bundle\FrameworkBundle\Tests\Templating\Helper\Fixtures\StubTemplateNameParser
+ *
+ * Needed to load the templates used for rendering form items.
+ */
+class stubTemplateNameParser implements TemplateNameParserInterface
+{
+    private $root;
+
+    private $rootTheme;
+
+    public function __construct($root, $rootTheme)
+    {
+        $this->root = $root;
+        $this->rootTheme = $rootTheme;
+    }
+
+    public function parse($name)
+    {
+
+      list($bundle, $controller, $template) = explode(':', $name);
+
+      if ($template[0] == '_') {
+          $path = $this->rootTheme.'/Custom/'.$template;
+      } elseif ($bundle === 'TestBundle') {
+          $path = $this->rootTheme.'/'.$controller.'/'.$template;
+      } else {
+          $path = $this->root.'/'.$controller.'/'.$template;
+      }
+
+      return new TemplateReference($path, 'php');
+    }
+}
